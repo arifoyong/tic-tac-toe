074c02216271db9328032b2fd5adc646642c8494
Arif <arifoyong@email.com>
2019-01-06 21:48:57 +0800

    Change color based on player

--- src/components/square.js
+++ src/components/square.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './square.css';
 
 class Square extends Component {
-     render() {
+    constructor(props) {
+        super(props);
+        this.handlePlayer = this.handlePlayer.bind(this);
+    }
+
+    handlePlayer(value) {
+        const valueIs = "valueIs" + value;
+        if (value === 'X') {
+            return <span className={valueIs}>{value}</span>
+        } 
+        
+        return <span className={valueIs}>{value}</span>
+    }
+
+    render() {
         return (
             <td className="square" onClick={this.props.onClick}>
-                {this.props.value}
+                {this.handlePlayer(this.props.value)}
             </td>
         );
     }
